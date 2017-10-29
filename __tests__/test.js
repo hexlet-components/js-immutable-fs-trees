3f58179c136135e050730b1825baf8dcbf143458
Kirill Mokevnin <mokevnin@gmail.com>
2017-10-29 15:15:21 +0100

    add hof

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { mkdir, mkfile } from '../src';
+import { mkdir, mkfile, map, reduce, filter } from '../src';
 
 test('build', () => {
   const tree = mkdir('/', [mkdir('etc'), mkdir('usr'), mkfile('robots.txt')]);
@@ -29,4 +29,108 @@ test('build', () => {
     name: '/',
     type: 'directory',
   });
+});
+
+test('reduce', () => {
+  const tree = mkdir('/', [
+    mkdir('eTc', [
+      mkdir('NgiNx'),
+      mkdir('CONSUL', [
+        mkfile('config.json'),
+      ]),
+    ]),
+    mkfile('hOsts'),
+  ]);
+  const actual = reduce((n, acc) => acc + 1, tree, 0);
+  expect(actual).toEqual(6);
+
+  const actual2 = reduce((n, acc) => (n.type === 'file' ? acc + 1 : acc), tree, 0);
+  expect(actual2).toEqual(2);
+
+  const actual3 = reduce((n, acc) => (n.type === 'directory' ? acc + 1 : acc), tree, 0);
+  expect(actual3).toEqual(4);
+});
+
+test('map', () => {
+  const tree = mkdir('/', [
+    mkdir('eTc', [
+      mkdir('NgiNx'),
+      mkdir('CONSUL', [
+        mkfile('config.json'),
+      ]),
+    ]),
+    mkfile('hOsts'),
+  ]);
+  const actual = map(n => ({ ...n, name: n.name.toUpperCase() }), tree);
+
+  const expected = {
+    children: [
+      {
+        children: [
+          {
+            children: [], meta: {}, name: 'NGINX', type: 'directory',
+          },
+          {
+            children: [{ meta: {}, name: 'CONFIG.JSON', type: 'file' }],
+            meta: {},
+            name: 'CONSUL',
+            type: 'directory',
+          },
+        ],
+        meta: {},
+        name: 'ETC',
+        type: 'directory',
+      },
+      { meta: {}, name: 'HOSTS', type: 'file' },
+    ],
+    meta: {},
+    name: '/',
+    type: 'directory',
+  };
+
+  expect(actual).toEqual(expected);
+});
+
+test('filter', () => {
+  const tree = mkdir('/', [
+    mkdir('etc', [
+      mkdir('nginx', [
+        mkdir('conf.d'),
+      ]),
+      mkdir('consul', [
+        mkfile('config.json'),
+      ]),
+    ]),
+    mkfile('hosts'),
+  ]);
+  const actual = filter(n => n.type === 'directory', tree);
+
+  const expected = {
+    children: [
+      {
+        children: [
+          {
+            children: [{ children: [], meta: {}, name: 'conf.d', type: 'directory' }],
+            meta: {},
+            name: 'nginx',
+            type: 'directory',
+          },
+          {
+            children: [],
+            meta: {},
+            name: 'consul',
+            type: 'directory',
+          },
+        ],
+        meta: {},
+        name: 'etc',
+        type: 'directory',
+      },
+    ],
+    meta: {},
+    name: '/',
+    type: 'directory',
+  };
+
+  expect(actual).toEqual(expected);
 });
--- src/index.js
+++ src/index.js
@@ -2,15 +2,54 @@
 
 import 'source-map-support/register';
 
-export const mkdir = (name: string, children: Array<{}> = [], meta: Object = {}) => ({
+type BaseNode = { name: string, meta: {} };
+type File = BaseNode & {
+  type: 'file'
+};
+
+/* eslint-disable no-use-before-define */
+type Directory = BaseNode & {
+  type: 'directory',
+  children: Array<Node>
+};
+/* eslint-enable no-use-before-define */
+
+type Node = File | Directory;
+
+export const mkdir = (name: string, children: Array<Node> = [], meta: Object = {}): Node => ({
   name,
   children,
   meta,
   type: 'directory',
 });
 
-export const mkfile = (name: string, meta: Object = {}) => ({
+export const mkfile = (name: string, meta: Object = {}): Node => ({
   name,
   meta,
   type: 'file',
 });
+
+export const map = (f: Node => any, node: Node) => {
+  const updatedNode = f(node);
+
+  return node.type === 'directory' ?
+    { ...updatedNode, children: (node.children || []).map(n => map(f, n)) } : updatedNode;
+};
+
+export const reduce = <T>(f: (Node, T) => T, node: Node, acc: T): T => {
+  const newAcc = f(node, acc);
+
+  if (node.type === 'file') {
+    return newAcc;
+  }
+  return (node.children || []).reduce((iAcc, n) => reduce(f, n, iAcc), newAcc);
+};
+
+export const filter = (f: Node => boolean, node: Node) => {
+  if (!f(node)) {
+    return null;
+  }
+
+  return node.type === 'directory' ?
+    { ...node, children: (node.children || []).map(n => filter(f, n)).filter(v => v) } : node;
+};
